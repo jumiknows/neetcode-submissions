7ab50ccd92d49a66b8751ef5cef6bf0681af2e93
Ernest Wong <146879533+jumiknows@users.noreply.github.com>
2026-09-25 14:31:32 -0700

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # transforms set to grab all unique values only
+        check = set(nums)
+        print(check)
+        print(len(check))
+        print(len(nums))
+        # find the length of unique values
+        check_length = len(check)
+        # if unique value length equals nums list length, return false
+        if (check_length == len(nums)):
+            return False
+        # otherwise return true
+        else:
+            return True
